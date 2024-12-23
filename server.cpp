67a5d1c409f1e8e59d5e735a2acbcbc51582752f
Olek <olek.czarnecki@gmail.com>
2024-12-23 22:28:24 +0100

    dodano obsługę opuszczania serwera przez graczy

--- server.cpp
+++ server.cpp
@@ -4,6 +4,29 @@
 #define MAX_PLAYERS 10
 int players_num = 1;
 
+
+
+
+void printdesclist(pollfd *pfds, int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        printf("fd: %d, events: %d, revents: %d\n", pfds[i].fd, pfds[i].events, pfds[i].revents);
+    }
+}
+
+void remove_player(pollfd *pfds, int index, int &players_num)
+{
+    close(pfds[index].fd);
+    pfds[index].fd = -1;
+    for (int i = index; i < players_num - 1; i++)
+    {
+        pfds[i] = pfds[i + 1];
+    }
+    pfds[players_num - 1].fd = -1;
+    players_num--;
+}
+
 int main(int argc, char **argv)
 {
     int servSock = socket_bind_listen(argc, argv, SOCK_STREAM);
@@ -20,7 +43,11 @@ int main(int argc, char **argv)
     pollfd pfds[MAX_PLAYERS]{};
     pfds[0].fd = servSock;
     pfds[0].events = POLLIN;
-    
+    for(int i=1; i<MAX_PLAYERS; i++)
+    {
+        pfds[i].fd = -1;
+    }
+
     while(1)
     {
         sockaddr_in client_addr;
@@ -43,20 +70,36 @@ int main(int argc, char **argv)
                     perror("problem z accept.");
                     exit(1);
                 }
-                printf("Gracz %d dolaczyl do gry.\n", players_num);
-                pfds[players_num].fd = player_fd;
-                pfds[players_num].events = POLLIN;
+                printf("Gracz %d dolaczyl do gry.\n", player_fd - 3);
+
+                for(int i=1; i<MAX_PLAYERS; i++)
+                {
+                    if(pfds[i].fd == -1)
+                    {
+                        pfds[i].fd = player_fd;
+                        pfds[i].events = POLLIN;
+                        break;
+                    }
+                }
                 players_num++;
 
                 write(player_fd, a, sizeof(a)); 
             }
 
-            for(int i=1; i<MAX_PLAYERS; i++)
+            for(int i=1; i<players_num; i++)
             {
                   if (pfds[i].fd > 0 && pfds[i].revents & POLLIN)
                   {
                     char buffer[128];
-                    read(pfds[i].fd, buffer, sizeof(buffer));
+                    int bytes_read = read(pfds[i].fd, buffer, sizeof(buffer));
+
+                    if(bytes_read == 0)
+                    {
+                        printf("Gracz %d opuscil gre.\n", pfds[i].fd - 3);
+                        remove_player(pfds, i, players_num);
+                        //printdesclist(pfds, MAX_PLAYERS);
+                        continue;
+                    }
                     printf("Wiadomosc od gracza %d: %s \n",i,buffer);
                   }
             }
@@ -64,3 +107,4 @@ int main(int argc, char **argv)
     }
 
 }
+
