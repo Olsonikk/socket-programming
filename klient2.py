50876ad8def1d3b4624e8c2e7d8dae88e9b0ad70
Igor Simon <75307125+HOZY39@users.noreply.github.com>
2025-01-06 15:25:09 +0000

    Update klienta

--- klient2.py
+++ klient2.py
@@ -14,6 +14,7 @@ class MathQuizClient:
         self.client_socket = None
         self.server_address = server_address
 
+        self.flag = True
         self.connect_to_server()
 
 
@@ -28,6 +29,9 @@ class MathQuizClient:
             print("Could not connect to server")
             self.root.quit()
     def create_nick_entry(self):
+    
+        data = self.client_socket.recv(1024).decode()
+        print(data)
         for widget in self.root.winfo_children():
             widget.destroy()
 
@@ -40,15 +44,13 @@ class MathQuizClient:
         self.nick_entry.focus()
 
     def submit_nick(self):
-        #######################################
-
-
-
-        #######################################
-        Serwer.send(["CreatePlayer", self.nick_entry.get()])
-        print(Serwer.send("PlayerList"))
-        self.nick = self.nick_entry.get()
+        #Serwer.send(["CreatePlayer", self.nick_entry.get()])
+        #print(Serwer.send("PlayerList"))
+        self.nick = self.nick_entry.get().strip()
         if self.nick:
+            self.client_socket.sendall(self.nick.encode('utf-8') + b'\n')
+            data = self.client_socket.recv(1024).decode()
+            print(data)
             self.choose_room_option()  # Po podaniu nicku, gracz może wybrać pokój
 
     def choose_room_option(self):
@@ -61,6 +63,10 @@ class MathQuizClient:
         tk.Button(self.root, text="Join Room", command=self.show_available_rooms).pack(pady=10)
 
     def create_room(self):
+        self.client_socket.sendall('2'.encode('utf-8') + b'\n')
+        data = self.client_socket.recv(1024).decode()
+        print(data)
+        
         for widget in self.root.winfo_children():
             widget.destroy()
 
@@ -77,47 +83,90 @@ class MathQuizClient:
         self.room_entry.focus()
 
     def submit_create_room(self):
-        #######################################
-
-
-
-
-        #######################################
         if self.room_entry.get() in Serwer.send("RoomList"):
             self.RoomExistError.config(text="This room already exist!")
             #time.sleep(2)
             #self.create_room()
         else:
-            Serwer.send(["CreateRoom", self.room_entry.get()])
-            print(Serwer.send("RoomList"))
+            #Serwer.send(["CreateRoom", self.room_entry.get()])
+            #print(Serwer.send("RoomList"))
             self.room = self.room_entry.get()
-            self.show_room_menu()
+            self.client_socket.sendall(self.room.encode('utf-8') + b'\n')
+            data = self.client_socket.recv(1024).decode()
+            print(data)
+            self.client_socket.sendall('1'.encode('utf-8') + b'\n')
+            data = self.client_socket.recv(1024).decode()
+            data = data.splitlines()[0].split(" ", 2)
+            print(data)
+            if data[2]==self.room:
+                    print('jestem tutaj')
+                    print(data[0])
+                    self.client_socket.sendall(data[0].encode('utf-8') + b'\n')
+                    data = self.client_socket.recv(1024).decode()
+                    print(data)
+                    data = self.client_socket.recv(1024).decode()
+                    print(data)
+                    self.choose_room_option()
+            else:
+                print("QGYHUIJKOLP")
+                data = self.client_socket.recv(1024).decode()
+                print(data)
+                print(data.splitlines())
+                for line in data.splitlines():
+                    parts = line.split(" ", 2)
+                    print(parts)
+                    if parts[2]==self.room:
+                        self.client_socket.sendall(parts[0].encode('utf-8') + b'\n')
+                        data = self.client_socket.recv(1024).decode()
+                        print(data)
+                        
+                self.choose_room_option()
+                #self.show_room_menu()
 
     def show_available_rooms(self):
         """Pokazuje dostępne pokoje"""
+        
+        self.rooms=[]
+        self.client_socket.sendall('1'.encode('utf-8') + b'\n')
+        time.sleep(1)
+        data = self.client_socket.recv(1024).decode()
+        #data = data.splitlines()[0].split(" ", 2)
+        print(data.splitlines())
+        test = data.splitlines()[0][0]
+        if test=='1':
+            for line in data.splitlines():
+                parts = line.split(" ", 2)
+                if len(parts)==3:
+                    print(parts)
+                    self.rooms.append(parts[2])
+        else:
+            self.choose_room_option
+            return
+        self.rooms.pop()
         for widget in self.root.winfo_children():
             widget.destroy()
 
         tk.Button(self.root, text="back", command=self.choose_room_option).place(x=0,y=0)
         tk.Label(self.root, text="Available rooms:", font=("Arial", 16)).pack(pady=20)
 
-        #######################################
-
-
-
-
-        #######################################
-        rooms = Serwer.send("RoomList")
+        print("rooms: \n")
+        print(self.rooms)
+        #rooms = Serwer.send("RoomList")
         #rooms = ["Spelarnia", "Sigmoza", "Husaria"]
-        for room_name in rooms:
+        for i, room_name in enumerate(self.rooms):
             frame = tk.Frame(self.root)
             frame.pack(pady=5)
             tk.Label(frame, text=room_name, font=("Arial", 14)).grid(row=0, column=0, padx=10)
-            tk.Button(frame, text=f"Join {room_name}", command=lambda room_name=room_name: self.join_room(room_name)).grid(row=0, column=1, padx=10)
+            tk.Button(frame, text=f"Join {room_name}", command=lambda room_name=room_name, roomId=i+1: self.join_room(room_name, roomId)).grid(row=0, column=1, padx=10)
 
-    def join_room(self, roomName):
+    def join_room(self, roomName, roomId):
         """Dołącza do pokoju"""
         room_name = roomName
+        room_id = roomId
+        self.client_socket.sendall(str(room_id).encode('utf-8') + b'\n')
+        time.sleep(0.5)
+        data = self.client_socket.recv(1024).decode()
+        print(data)
         self.room = room_name
         self.show_room_menu()
 
